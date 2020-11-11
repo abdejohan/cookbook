b459043aba617ca6ae4dc84e5eed64e465283aa5
Johan Abde <abdejohan@gmail.com>
2020-11-11 16:35:58 +0100

    Clean Note component from unused code

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,22 +1,9 @@
-/* eslint-disable no-unused-vars */
 import React from "react";
 // import { withRouter } from "react-router-dom";
 import { useForm } from "react-hook-form";
-// mport { makeStyles, useTheme } from "@material-ui/core/styles";
-// import Typography from "@material-ui/core/Typography";
-import Paper from "@material-ui/core/Paper";
-import TextField from "@material-ui/core/TextField";
-import { withRouter } from "react-router-dom";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
+import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
-import Menu from "@material-ui/core/Menu";
-import Button from "@material-ui/core/Button";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
+
 // import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles(() => ({
@@ -55,8 +42,8 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
-const Note = (props) => {
-  const { register, handleSubmit, errors } = useForm();
+const Note = () => {
+  const { handleSubmit } = useForm();
   const onSubmit = (data) => console.log(data);
   const classes = useStyles();
 
