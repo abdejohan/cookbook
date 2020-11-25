109d7824a5a7e9dac40f6b7ad2a324eb609c6918
Johan Abde <abdejohan@gmail.com>
2020-11-25 17:51:34 +0100

    Move search logic from Home.jsx to its own component

---
+++ src/components/SearchList.jsx
@@ -0,0 +1,81 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React, { useState, useContext, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+import UserContext from "../context/UserContext";
+import ListItem from "./ListItem";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  searchField: {
+    marginBottom: "20px",
+  },
+}));
+
+const SearchList = (props) => {
+  const { searchInput } = props;
+  const [searchResponseList, setSearchResponseList] = useState([]);
+  const [userSearchResponseList, setUserSearchResponseList] = useState([]);
+  const classes = useStyles();
+  const history = useHistory();
+  const { userData } = useContext(UserContext);
+
+  useEffect(() => {
+    const getSearchResults = async () => {
+      try {
+        const searchRequest = await axios.get(
+          `http://localhost:5000/search?term=${searchInput}`
+        );
+
+        const userSearchRequest = await axios.get(
+          `http://localhost:5000/search/user?term=${searchInput}`
+        );
+
+        setSearchResponseList(searchRequest.data);
+        setUserSearchResponseList(userSearchRequest.data);
+      } catch (error) {
+        setSearchResponseList(0);
+        setUserSearchResponseList(0);
+        console.log(`THIS MESSAGE:${error}`);
+      }
+    };
+    getSearchResults();
+  }, [searchInput]);
+
+  useEffect(() => {
+    if (!userData.user) {
+      history.push("/login");
+    }
+  });
+
+  return (
+    <div>
+      <section>
+        <div>
+          {searchResponseList.length > 0 && (
+            <>
+              <ul>
+                <h4>Recipes found..</h4>
+                <ListItem searchResponseList={searchResponseList} />
+              </ul>
+            </>
+          )}
+          {userSearchResponseList.length > 0 && (
+            <>
+              <ul>
+                <h4>Users found..</h4>
+                <ListItem userSearchResponseList={userSearchResponseList} />
+              </ul>
+            </>
+          )}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default SearchList;
