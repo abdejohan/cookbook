97dea92d2ac375dfa4fad7818aef6acdd1dcfb24
Johan Abde <abdejohan@gmail.com>
2021-01-22 22:30:07 +0100

    Style about.js Add content text

--- pages/about.js
+++ pages/about.js
@@ -3,11 +3,21 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(() => ({
   article: {
-    maxWidth: "600px",
+    marginTop: "20px",
+    maxWidth: "500px",
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
     flexFlow: "column nowrap",
+  },
+  header: {
+    marginBottom: "50px",
+  },
+  orange: {
+    color: "orange",
+  },
+  blue: {
+    color: "blue",
   },
 }));
 
@@ -15,20 +25,26 @@ const About = () => {
   const classes = useStyles();
   return (
     <article className={classes.article}>
-      <h5 className="page-header">About Us</h5>
+      <h5 className={`page-header ${classes.header}`}>About Us</h5>
       <p className="plain-text">
-        Stop saving recipes in brower tabs, stop with screenshots of old notes
-        your grandmother gave you.{" "}
+        <span className={classes.orange}>Stop</span> storing recipes in brower
+        tabs you know your not going to revisit.{" "}
+        <span className={classes.orange}>Stop</span> taking screenshots of notes
+        your grandmother gave you that you know at some point when your phone
+        runs out of space{" "}
       </p>
       <br />
       <p className="plain-text">
-        And if you are a content creator with a follower base, then dont let
-        your wonderful recipes disappear as a caption to your posts.
+        And if you are a content creator with a follower base, then{" "}
+        <span className={classes.orange}>don´t</span> let your wonderful recipes
+        disappear as a caption to your instagram posts.
       </p>
       <br />
       <p className="plain-text">
-        Create your own Virtual cookbook here and store them where other people
-        will be able to find them.
+        Instead here you have the ability to store all of them at the same place
+        while having it easily <span className={classes.blue}>accessible</span>.{" "}
+        <br /> <span className={classes.blue}>Create</span> your own Virtual
+        cookbook and store your recipes so both you and others can find them!
       </p>
     </article>
   );
